7ee4c642e585b4ae73c6e189fe66e7bc9a971e71
Ronald Kolum <kolumronnie@gmail.com>
2022-02-28 16:19:48 +0300

    Added dependencies

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,9 @@
+import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
 }
