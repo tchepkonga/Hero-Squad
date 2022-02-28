657bad40c0c57a1bf0800cef9bdcd8e92097e24c
Ronald Kolum <kolumronnie@gmail.com>
2022-02-28 20:54:03 +0300

    Implemented heroes model and view

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,11 +5,26 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+
 public class App {
     public static void main(String[] args) {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-           return new ModelAndView(model, "heroes.hbs");
+//            createHeroes();
+//            ArrayList<Hero> heroes = Hero.getAll();
+//            model.put("heroes", heroes);
+           return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/heroes", (request, response) -> {
+            Map <String, Object> model = new HashMap<>();
+            Hero.createHeroes();
+            ArrayList<Hero> heroes = Hero.getAll();
+//            System.out.println(heroes);
+//            Hero hero1 = new Hero("Superman", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+            request.session().attribute("heroes",heroes);
+            model.put("heroes", request.session().attribute("heroes"));
+            return new ModelAndView(model, "heroes.hbs");
         }, new HandlebarsTemplateEngine());
     }
+
 }
--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -18,4 +18,41 @@ public class Hero {
         instances.add(this);
         heroId++;
     }
+    public static ArrayList<Hero> createHeroes(){
+        Hero hero1 = new Hero("Superman", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+        Hero hero2= new Hero("Superman2", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+        Hero hero3 = new Hero("Superman3", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+        Hero hero4 = new Hero("Superman4", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+        Hero hero5 = new Hero("Superman5", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+        Hero hero6 = new Hero("Superman6", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg");
+        return Hero.getAll();
+    }
+
+    public static int getHeroId() {
+        return heroId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialPowers() {
+        return specialPowers;
+    }
+
+    public String getWeaknesses() {
+        return weaknesses;
+    }
+
+    public static ArrayList<Hero> getAll(){
+        return instances;
+    }
 }
