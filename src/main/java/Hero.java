3da2e265c669665dc097bfa8c1edd441b8dcb85e
Ronald Kolum <kolumronnie@gmail.com>
2022-02-28 16:10:04 +0300

    Declared Hero attributes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,5 +1,20 @@
 import java.util.ArrayList;
 
 public class Hero {
+    private static int heroID;
+    private String name;
+    private String image;
+    private static final ArrayList<Hero> instances = new ArrayList<>();
+    private int age;
+    private String specialPowers;
+    private String weaknesses;
 
+    public Hero(int heroID,String name, int age, String specialPowers, String weaknesses, String image) {
+        this.name = name;
+        this.age = age;
+        this.specialPowers = specialPowers;
+        this.weaknesses = weaknesses;
+        this.image = image;
+        instances.add(this);
+    }
 }
