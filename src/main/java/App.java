5961b92f4a2750e4baac551c3d061c5bebe1d76f
Ronald Kolum <kolumronnie@gmail.com>
2022-03-02 03:58:18 +0300

    Added another template and moved the squads form there

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,14 +13,18 @@ public class App {
 
            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
-        get("/heroes", (request, response) -> {
-            Map <String, Object> model = new HashMap<>();
-            Hero.createHeroes();
-            ArrayList<Hero> heroes = Hero.getAll();
-            request.session().attribute("heroes",heroes);
-            model.put("heroes", request.session().attribute("heroes"));
-            return new ModelAndView(model, "heroes.hbs");
+        get("/squads-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squads-form.hbs");
         }, new HandlebarsTemplateEngine());
+//        get("/heroes", (request, response) -> {
+//            Map <String, Object> model = new HashMap<>();
+//            Hero.createHeroes();
+//            ArrayList<Hero> heroes = Hero.getAll();
+//            request.session().attribute("heroes",heroes);
+//            model.put("heroes", request.session().attribute("heroes"));
+//            return new ModelAndView(model, "heroes.hbs");
+//        }, new HandlebarsTemplateEngine());
         get("/squads", (request, response) -> {
             Map <String, Object> model = new HashMap<>();
             request.session().attribute("squads", Squad.getAll());
