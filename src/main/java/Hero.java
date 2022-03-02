bff8ea760cbe404221a0a5ec7c4ef03a33d7ba64
Ronald Kolum <kolumronnie@gmail.com>
2022-03-02 03:39:45 +0300

    Updated class Hero

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,54 +1,25 @@
 import java.util.ArrayList;
 
 public class Hero {
-    private static int heroId= 0;
+    private int mId= 0;
     private String name;
-    private String image;
     private static final ArrayList<Hero> instances = new ArrayList<>();
     private int age;
     private String specialPowers;
     private String weaknesses;
-    private String squad;
 
 
-    public Hero(String name, int age, String specialPowers, String weaknesses, String image, String squad) {
+    public Hero(String name, int age, String specialPowers, String weaknesses) {
         this.name = name;
         this.age = age;
         this.specialPowers = specialPowers;
         this.weaknesses = weaknesses;
-        this.image = image;
-        this.squad = squad;
         instances.add(this);
-        heroId++;
-    }
-    public static ArrayList<Hero> createHeroes(){
-        Hero hero1 = new Hero("Superman", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg","Titans");
-        Hero hero2= new Hero("Superman2", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg","Titans");
-        Hero hero3 = new Hero("Superman3", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg","Titans");
-        Hero hero4 = new Hero("Superman4", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg", "TMNT");
-        Hero hero5 = new Hero("Superman5", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg", "TMNT");
-        Hero hero6 = new Hero("Superman6", 30,  "Strength","love", "https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg", "TMNT");
-        return Hero.getAll();
-    }
-
-    public String getSquad() {
-        return squad;
-    }
-
-    public void setSquad(String squad) {
-        this.squad = squad;
-    }
-
-    public static int getHeroId() {
-        return heroId;
+        mId = instances.size();
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getImage() {
-        return image;
     }
 
     public int getAge() {
@@ -66,4 +37,8 @@ public class Hero {
     public static ArrayList<Hero> getAll(){
         return instances;
     }
+
+    public static void  clear(){instances.clear();}
+
+    public static Hero findHero(int id){return instances.get(id-1);}
 }
